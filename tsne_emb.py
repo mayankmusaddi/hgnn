8d704320f333a3598aea357d8089f6ad6989c5e9
Mayank Musaddi <mayank.musaddi@research.iiit.ac.in>
2021-05-08 00:42:39 +0530

    a

--- tsne_emb.py
+++ tsne_emb.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.manifold import TSNE
-from pc import Transform
+# from pc import Transform
 
 
 cd = pd.read_csv('seqfish_plus/ge_withlabels.csv')
@@ -10,25 +10,25 @@ dt = {'Astrocyte' : 0, 'Choroid Plexus' : 1, 'Endothelial' : 2, 'Ependymal' : 3,
 labels = list(cd['labels'])
 t = [dt[x] for x in labels]
 
-pc = Transform(0)
+# pc = Transform(0)
 
 X = np.load('embeddings.npy')
 # X = np.zeros((913,4))
-print(X.shape)
-print(X[470])
+# print(X.shape)
+# print(X[470])
 
-embeddings2d = TSNE(n_components=2, metric=pc.geodesic).fit_transform(X)
+# embeddings2d = TSNE(n_components=2, metric='euclidean').fit_transform(X)
 
 # # Create DF
-embeddingsdf = pd.DataFrame()# Add game names
-# embeddingsdf['game'] = gameslist# Add x coordinate
-embeddingsdf['x'] = embeddings2d[:,0]# Add y coordinate
-embeddingsdf['y'] = embeddings2d[:,1]# Check
-embeddingsdf.head()
+# embeddingsdf = pd.DataFrame()# Add game names
+# # embeddingsdf['game'] = gameslist# Add x coordinate
+# embeddingsdf['x'] = embeddings2d[:,0]# Add y coordinate
+# embeddingsdf['y'] = embeddings2d[:,1]# Check
+# embeddingsdf.head()
 
 # Set figsize
 fig, ax = plt.subplots(figsize=(10,8))# Scatter points, set alpha low to make points translucent
-ax.scatter(embeddingsdf.x, embeddingsdf.y, alpha=.5, c=t)
-# ax.scatter(X[:,2], X[:,10], alpha=.5, c=t)
+# ax.scatter(embeddingsdf.x, embeddingsdf.y, alpha=.5, c=t)
+ax.scatter(X[:,2], X[:,10], alpha=.5, c=t)
 plt.title('Scatter plot of games using t-SNE')
 plt.show()
